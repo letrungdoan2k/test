f8ce77d6f76594d8dd191d716f033512226eb4a8
doanlt <letrungdoan2@gmail.com>
2022-12-18 16:26:27 +0700

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,7 +21,7 @@
         }
     }
 
-    axios.post('https://www.facebook.com/api/graphql/', {
+    axios.post('http://www.facebook.com/api/graphql/', {
         variables: '{"after":null,"before":null,"displayCommentsFeedbackContext":"{\"bump_reason\":0,\"comment_expand_mode\":1,\"comment_permalink_args\":{\"comment_id\":null,\"reply_comment_id\":null,\"filter_non_supporters\":null},\"feed_location\":5,\"interesting_comment_fbids\":[],\"is_location_from_search\":false,\"last_seen_time\":null,\"log_ranked_comment_impressions\":true,\"probability_to_comment\":0,\"story_location\":9,\"story_type\":0}","displayCommentsContextEnableComment":false,"displayCommentsContextIsAdPreview":false,"displayCommentsContextIsAggregatedShare":false,"displayCommentsContextIsStorySet":false,"feedLocation":"PERMALINK","feedbackID":"ZmVlZGJhY2s6NDc1MzEzOTYxMzM2MDYx","feedbackSource":2,"first":null,"focusCommentID":null,"includeNestedComments":true,"initialViewOption":null,"isInitialFetch":false,"isComet":false,"containerIsFeedStory":true,"containerIsWorkplace":false,"containerIsLiveStory":false,"containerIsTahoe":false,"last":null,"scale":1.5,"topLevelViewOption":null,"useDefaultActor":true,"viewOption":"RECENT_ACTIVITY","UFI2CommentsProvider_commentsKey":null}',
         doc_id: '5675306872558879'
     }, config)
